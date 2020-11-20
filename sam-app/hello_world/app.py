d9f021862f9b66cfd3ef46a89feae97c8476f304
utsumi kentaro <utsumi.kentaro@nifty.co.jp>
2020-11-20 11:05:22 +0900

    s

--- sam-app/hello_world/app.py
+++ sam-app/hello_world/app.py
@@ -5,6 +5,6 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world48",
+            "message": "hello world49",
         }),
     }
