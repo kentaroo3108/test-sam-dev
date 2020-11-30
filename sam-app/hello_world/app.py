dd12264d0f7b5f122f7df80fbad7ddf204a844a5
utsumi kentaro <utsumi.kentaro@nifty.co.jp>
2020-11-30 15:14:20 +0900

    s

--- sam-app/hello_world/app.py
+++ sam-app/hello_world/app.py
@@ -3,7 +3,7 @@ import boto3
 from datetime import datetime
 import botocore
 
-ec2_client = boto3.client('ec2', region_name='ap-northeast-1')
+ec2_client = boto3.client('ec2', region_name='ap-northeast-1')a
 ec2_resource = boto3.resource('ec2', region_name='ap-northeast-1')
 
 
