7b6808fe8c5149726a95f5d62da6c2cd539e1089
utsumi kentaro <utsumi.kentaro@nifty.co.jp>
2020-11-30 14:36:08 +0900

    ed

--- sam-app/hello_world/app.py
+++ sam-app/hello_world/app.py
@@ -1,58 +1,10 @@
 import json
-import boto3
-from datetime import datetime
-import botocore
-
-ec2_client = boto3.client('ec2', region_name='ap-northeast-1')
-ec2_resource = boto3.resource('ec2', region_name='ap-northeast-1')
-
-
-def create_ami(instance_id):
-    try:
-        instance = ec2_resource.Instance(instance_id)
-        image = instance.create_image(
-            Name=f'{instance_id}_{datetime.now().strftime("%Y%m%d%H%M%S")}',
-            NoReboot=True
-        )
-        image.create_tags(
-            Tags=[
-                {
-                    'Key': 'Name',
-                    'Value': "Hoge"
-                },
-                {
-                    'Key': 'Origin',
-                    'Value': instance.id
-                },
-                {
-                    'Key': 'env',
-                    'Value': 'prod'
-                }
-            ]
-        )
-        #iimage.wait_until_exists(Filters=[{'Name': 'state', 'Values': ['available']}])
-        launch_template_name = 'test-auto-20201030'
-        # latest_launch_template_version_num = launch_template['LaunchTemplates'][0]['LatestVersionNumber']print(latest_launch_template_version_num)
-        launch_template_version = ec2_client.describe_launch_template_versions(
-            LaunchTemplateName=launch_template_name,
-        )
-        launch_template_data = launch_template_version['LaunchTemplateVersions'][0]['LaunchTemplateData']
-        launch_template_data['ImageId'] = image.id
-        new_launch_template_version = ec2_client.create_launch_template_version(
-            LaunchTemplateName=launch_template_name,
-            LaunchTemplateData=launch_template_data
-        )
-        print(new_launch_template_version)
-        new_launch_template_version_number = new_launch_template_version[
-            'LaunchTemplateVersion']['VersionNumber']
-        ec2_client.modify_launch_template(
-            LaunchTemplateName=launch_template_name,
-            DefaultVersion=str(new_launch_template_version_number)
-        )
-    except Exception as e:
-        print(e)
 
 
 def lambda_handler(event, context):
-    #instance_id = event['instance_id']
-    create_ami("i-09cd6f52214720fec")
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "hello world1",
+        }),
+    }
