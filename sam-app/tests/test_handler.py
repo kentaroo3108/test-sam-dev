570095ff3b1bfe2b1f917fd6542fc430fe9ca887
utsumi kentaro <utsumi.kentaro@nifty.co.jp>
2021-01-13 15:49:11 +0900

    s

--- sam-app/tests/test_handler.py
+++ sam-app/tests/test_handler.py
@@ -1,9 +1,11 @@
-from hello_world import app
+import json
+
 import pytest
-=import json
+
+from launch_update import app
 
 
-@pytest.fixture()aa
+@pytest.fixture()
 def apigw_event():
     """ Generates API GW Event"""
 
@@ -34,19 +36,22 @@ def apigw_event():
         },
         "queryStringParameters": {"foo": "bar"},
         "headers": {
-            "Via": "1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net (CloudFront)",
+            "Via": "1.1 08f323deadbeefa7af34d5feb414ce27 \
+                .cloudfront.net(CloudFront)",
             "Accept-Language": "en-US,en;q=0.8",
             "CloudFront-Is-Desktop-Viewer": "true",
             "CloudFront-Is-SmartTV-Viewer": "false",
             "CloudFront-Is-Mobile-Viewer": "false",
             "X-Forwarded-For": "127.0.0.1, 127.0.0.2",
             "CloudFront-Viewer-Country": "US",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9, \
+                image/webp,*/*;q=0.8",
             "Upgrade-Insecure-Requests": "1",
             "X-Forwarded-Port": "443",
             "Host": "1234567890.execute-api.us-east-1.amazonaws.com",
             "X-Forwarded-Proto": "https",
-            "X-Amz-Cf-Id": "aaaaaaaaaae3VYQb9jd-nvCd-de396Uhbp027Y2JvkCPNLmGJHqlaA==",
+            "X-Amz-Cf-Id": "aaaaaaaaaae3VYQb9jd-nvCd-de396Uhbp02 \
+                7Y2JvkCPNLmGJHqlaA==",
             "CloudFront-Is-Tablet-Viewer": "false",
             "Cache-Control": "max-age=0",
             "User-Agent": "Custom User Agent String",
